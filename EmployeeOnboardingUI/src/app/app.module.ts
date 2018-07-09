f81eb6178b09e018ed2881599f65c59376371a02
unknown <asha.polamarasetty@gmail.com>
2018-07-09 23:08:21 +0530

    Guidelines

--- EmployeeOnboardingUI/src/app/app.module.ts
+++ EmployeeOnboardingUI/src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { OnboardingComponent } from './onboarding/onboarding.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { GuidelinesComponent } from './guidelines/guidelines.component';
+import { CreateProfileComponent } from './create-profile/create-profile.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
+const routes:Routes=[
+  { path: '', redirectTo: '/CreateProfile', pathMatch: 'full' },
+  {path:'Guidelines',component:GuidelinesComponent},
+  {path:'CreateProfile',component:CreateProfileComponent},
+  {path:'ViewProfile',component:ViewProfileComponent},
+ ];
 @NgModule({
   declarations: [
     AppComponent,
     OnboardingComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    GuidelinesComponent,
+    CreateProfileComponent,
+    ViewProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes, {useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
